3a43e7be46fae5d751eb45208dd81e47dedbea9c
Brandon Clapp <brandonclapp@outlook.com>
2018-05-26 12:39:12 -0500

    Establish DB connection

--- Netify.SqlServer/SqlServerExtentions.cs
+++ Netify.SqlServer/SqlServerExtentions.cs
@@ -7,8 +7,6 @@ namespace Netify.SqlServer
     {
         public static IServiceCollection AddSqlServer(this IServiceCollection services, string connectionString)
         {
-            //factory.Get<PostAbstraction>((s) => new PostAbstraction());
-
             services.AddTransient(fac => new SqlServerDataAbstraction(connectionString));
             services.AddTransient<IPostDataAbstraction, PostAbstraction>();
 
