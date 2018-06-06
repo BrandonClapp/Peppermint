3df29df4796b89b70d349d508dcd30485db852f6
Brandon Clapp <brandonclapp@outlook.com>
2018-06-06 16:13:12 -0500

    Login working

--- Peppermint.App/Controllers/Account/AccountController.cs
+++ Peppermint.App/Controllers/Account/AccountController.cs
@@ -1,5 +1,10 @@
-﻿using Microsoft.AspNetCore.Mvc;
+﻿using Microsoft.AspNetCore.Authentication;
+using Microsoft.AspNetCore.Authentication.Cookies;
+using Microsoft.AspNetCore.Mvc;
 using Peppermint.App.Controllers.Account.Requests;
+using Peppermint.Core.Entities;
+using System.Collections.Generic;
+using System.Security.Claims;
 using System.Threading.Tasks;
 
 namespace Peppermint.App.Controllers.Account
@@ -7,6 +12,12 @@ namespace Peppermint.App.Controllers.Account
     [Route("[controller]")]
     public class AccountController : Controller
     {
+        private readonly EntityFactory _entityFactory;
+
+        public AccountController(EntityFactory entityFactory)
+        {
+            _entityFactory = entityFactory;
+        }
 
         [HttpGet("login")]
         public async Task<IActionResult> Login()
@@ -21,6 +32,24 @@ namespace Peppermint.App.Controllers.Account
             var temp = await Task.FromResult<dynamic>(new { });
 
             // validate credentials.
+            var user = await AuthenticateUser(credentials.UserName, credentials.Password);
+
+            if (user == null)
+                return View();
+
+            var claims = new List<Claim>
+                {
+                    new Claim(ClaimTypes.Email, user.Email),
+                    new Claim(ClaimTypes.Name, user.UserName)
+                };
+
+            var claimsIdentity = new ClaimsIdentity(
+                claims, CookieAuthenticationDefaults.AuthenticationScheme);
+
+
+            await HttpContext.SignInAsync(
+                    CookieAuthenticationDefaults.AuthenticationScheme,
+                    new ClaimsPrincipal(claimsIdentity));
 
             Request.Query.TryGetValue("returnUrl", out var returnUrl);
             if(returnUrl.Count == 0)
@@ -29,6 +58,25 @@ namespace Peppermint.App.Controllers.Account
             }
 
             return Redirect(returnUrl);
+        }
+
+        // Move to authentication service
+        private async Task<User> AuthenticateUser(string username, string password)
+        {
+            // Assume that checking the database takes 500ms
+            await Task.Delay(500);
+
+            if (username.ToLower() == "brandon" && password == "password")
+            {
+                var user = _entityFactory.Make<User>();
+                user.UserName = "Brandon";
+                user.Email = "brandon@email.com";
+                return user;
+            }
+            else
+            {
+                return null;
+            }
         }
     }
 }
