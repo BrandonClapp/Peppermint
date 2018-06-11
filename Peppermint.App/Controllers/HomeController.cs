02024d1e010499004f899f208296f0abd3e83ca8
Brandon Clapp <brandonclapp@outlook.com>
2018-06-10 20:51:52 -0500

    Page separation and home placement

---
+++ Peppermint.App/Controllers/HomeController.cs
@@ -0,0 +1,18 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace Peppermint.App.Controllers
+{
+    [Route("")]
+    public class HomeController : Controller
+    {
+        [HttpGet("")]
+        public IActionResult Index()
+        {
+            return View();
+        }
+    }
+}
