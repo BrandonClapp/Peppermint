341c69b5cd4fc93899a445224fa9bf326f9fff07
Brandon Clapp <brandonclapp@outlook.com>
2018-06-11 20:34:11 -0500

    Testing pagination on blog

--- Peppermint.App/Controllers/Blog/BlogController.cs
+++ Peppermint.App/Controllers/Blog/BlogController.cs
@@ -6,8 +6,10 @@ namespace Peppermint.App.Controllers.Blog
     [Route("[controller]")]
     public class BlogController : Controller
     {
+        [HttpGet("")]
         public IActionResult Index()
         {
+            Request.Query.TryGetValue("page", out var page);
             return View();
         }
     }
